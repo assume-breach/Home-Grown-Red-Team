cb6a2eeae00f494114b1aa9712e330665510c46c
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-03-24 17:28:57 -0400

    Update PiRepo.sh

--- PiRepo.sh
+++ PiRepo.sh
@@ -223,10 +223,20 @@ git clone https://github.com/hlldz/Phant0m.git
 git clone https://github.com/CCob/SharpBlock.git
 git clone https://github.com/Kharos102/NtdllUnpatcher.git
 git clone https://github.com/bats3c/DarkLoadLibrary.git 
+git clone https://github.com/Soledge/BlockEtw.git
+git clone https://github.com/mdsecactivebreach/firewalker.git
+git clone https://github.com/Cerbersec/KillDefenderBOF.git
 
+cd /Repos/Privilege_Escallation
 
-
-
-
-
+git clone https://github.com/0xthirteen/SharpStay.git
+git clone https://github.com/fireeye/SharPersist.git
+git clone https://github.com/outflanknl/SharpHide.git
+git clone https://github.com/Ben0xA/DoUCMe.git 
+git clone https://github.com/nccgroup/ABPTTS.git 
+git clone https://github.com/blackarrowsec/pivotnacci.git
+git clone https://github.com/sensepost/reGeorg.git
+git clone https://github.com/HarmJ0y/DAMP.git
+git clone https://github.com/0x09AL/IIS-Raid.git
+git clone https://github.com/antonioCoco/SharPyShell.git
 
