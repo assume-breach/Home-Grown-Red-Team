57af52bc0579562a3e5583b9963b31207220772c
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-04-27 11:48:01 -0400

    Update AutoC2.sh

--- AutoC2/AutoC2.sh
+++ AutoC2/AutoC2.sh
@@ -329,48 +329,183 @@ echo ""
 cd /opt/Payload_Development/
 echo "Installing PEzor"
 git clone https://github.com/phra/PEzor.git
-
+cd PEzor/
+bash install.sh
+echo ""
+read -p "Open A New Terminal And Export The Path For PEzor To Work!"
+echo ""
+sleep 2
+echo "Installing GadgetToJscript"
+echo ""
 git clone https://github.com/med0x2e/GadgetToJScript.git
+echo ""
+sleep 2
+echo "Installing ScareCrow"
+echo""
 git clone https://github.com/optiv/ScareCrow.git
+cd ScareCrow/
+go get github.com/fatih/color
+go get github.com/yeka/zip
+go get github.com/josephspurrier/goversioninfo
+apt install openssl -y
+apt install osslsigncode -y
+apt install mingw-w64 -y
+go build ScareCrow.go
+cd /opt/Payload_Development/
+echo ""
+sleep 2
+echo "Installing Donut"
+echo ""
 git clone https://github.com/TheWover/donut.git
+cd donut/
+python3 setup.py install
+cd /opt/Payload_Development
+mkdir MAC_OS
+cd MAC_OS
+echo ""
+sleep 2
+echo "Installing Mystikal"
+echo ""
 git clone https://github.com/D00MFist/Mystikal.git
+cd /opt/Payload_Development/
+echo ""
+sleep 2
+mkdir Windows_OS
+cd Windows_OS
+echo "Installing GadgetToJscript"
+git clone https://github.com/med0x2e/GadgetToJScript.git
+echo ""
+cd /opt/Payload_Development/
+echo "Installing Charlotte"
 git clone https://github.com/9emin1/charlotte.git
+echo ""
+cd /opt/Payload_Development/
+echo "Installing Invisibility Cloak"
 git clone https://github.com/xforcered/InvisibilityCloak.git
+echo ""
+cd /opt/Payload_Development/Windows_OS/
+echo "Installing Dendrobate"
+echo ""
 git clone https://github.com/FuzzySecurity/Dendrobate.git
+echo ""
+sleep 2
+cd /opt/Payload_Development/
+echo "Installing Offensive-VBA-and-XLS-Entanglement"
+echo ""
 git clone https://github.com/BC-SECURITY/Offensive-VBA-and-XLS-Entanglement.git
+sleep 2
+echo ""
+echo "Installing xlsGen"
+echo ""
+sleep 2
 git clone https://github.com/aaaddress1/xlsGen.git
+echo ""
+echo "Installing DarkArmour"
+echo ""
+sleep 2
 git clone https://github.com/bats3c/darkarmour.git
+sudo apt install mingw-w64-tools mingw-w64-common g++-mingw-w64 gcc-mingw-w64 upx-ucl osslsigncode
+echo ""
+echo "Installing InlineWhispers"
+echo""
+sleep 2
 git clone https://github.com/outflanknl/InlineWhispers.git
+echo ""
+cd /opt/Payload_Development/Windows_OS/
+echo "Installing EvilClippy"
+echo ""
+sleep 2
 git clone https://github.com/outflanknl/EvilClippy.git 
+echo ""
+echo "Installing OfficePurge"
+echo ""
 git clone https://github.com/fireeye/OfficePurge.git
+sleep 2
+echo ""
+echo "Installing ThreatCheck"
+echo ""
 git clone https://github.com/rasta-mouse/ThreatCheck.git
-git clone https://github.com/gloxec/CrossC2.git 
+echo ""
+echo "Ruler"
+echo ""
+sleep 2
 git clone https://github.com/sensepost/ruler.git
+echo ""
+echo "Installing DueDLLigence"
+echo ""
+sleep 2
 git clone https://github.com/fireeye/DueDLLigence.git
+echo ""
+echo "Installing RuralBishop"
+echo ""
+sleep 2
 git clone https://github.com/rasta-mouse/RuralBishop.git
+echo ""
+echo "Installing TikiTorch"
+echo ""
+sleep 2
 git clone https://github.com/rasta-mouse/TikiTorch.git 
+echo ""
+echo "Installing SharpShooter"
+echo ""
+sleep 2
 git clone https://github.com/mdsecactivebreach/SharpShooter.git
+echo ""
+echo "Installing SharpSploit"
+echo ""
+sleep 2
 git clone https://github.com/cobbr/SharpSploit.git
+echo ""
+echo "Installing MSBuildAPICaller"
+echo ""
+sleep 2
 git clone https://github.com/rvrsh3ll/MSBuildAPICaller.git
+echo ""
+echo "Installing Macro_Pack"
+echo ""
+sleep 2
 git clone https://github.com/sevagas/macro_pack.git
+echo ""
+echo "Installing Inceptor"
+echo ""
+sleep 2
 git clone https://github.com/klezVirus/inceptor.git
+echo ""
+echo "Installing Mortar
+echo ""
+sleep 2
 git clone https://github.com/0xsp-SRD/mortar.git
+echo ""
+echo "Installing RedTeamCCode"
+echo ""
+sleep 2
 git clone https://github.com/Mr-Un1k0d3r/RedTeamCCode.git
-
+echo ""
+sleep 2
 echo "Cloning Delivery Resources"
-
-cd ../Delivery
+echo ""
+cd /opt/Payload_Development/Delivery/
+echo ""
+echo "Installing O365 Attack Toolkit"
+echo ""
+sleep 2
 git clone https://github.com/mdsecactivebreach/o365-attack-toolkit.git
+echo ""
+sleep 2
+echo "
+echo "Installing BEEF"
+echo ""
+sleep 2
 git clone https://github.com/beefproject/beef.git
-
+cd beef
+./install
+echo ""
 echo "Cloning Your C2 Resources"
-
-cd ../Command_And_Control
-
+echo ""
+cd /opt/Command_And_Control
 echo "Cloning C2 Frameworks"
-
-mkdir C2_Frameworks
-cd C2_Frameworks
+echo ""
+echo "Installing Empire & Starkiller"
 git clone https://github.com/BC-SECURITY/Empire.git
 git clone https://github.com/nettitude/PoshC2.git
 git clone https://github.com/zerosum0x0/koadic.git
