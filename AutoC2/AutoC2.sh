5d3d2952a0c3eaf9a5bc1d5892897aad9d5a5a44
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-04-26 20:42:10 -0400

    Update AutoC2.sh

--- AutoC2/AutoC2.sh
+++ AutoC2/AutoC2.sh
@@ -252,13 +252,36 @@ echo ""
 echo "Cloning Initial Access Resources"
 echo ""
 sleep 2
-cd ../Initial_Access
+###Break For Recon Folder###
+cd /opt/Initial_Access
+echo "Installing Initial Access Tools"
+echo ""
+sleep 2
+echo "Installing Spraying Toolkit"
+echo ""
+sleep 2
 git clone https://github.com/byt3bl33d3r/SprayingToolkit.git
+cd SprayingToolkit/
+pip3 install -r requirements.txt
+cd /opt/Initial_Access
+echo ""
+sleep 2
+echo "Installing O365 Recon"
+echo ""
 git clone https://github.com/nyxgeek/o365recon.git
+echo ""
+sleep 2
+echo "Installing TREVORspray"
+echo ""
+sleep 2
 git clone https://github.com/blacklanternsecurity/TREVORspray.git
-
+cd TREVORspray/
+pip3 install -r requirements.txt
+sleep2
+echo ""
 echo "Cloning Payload Development Resources"
-
+echo ""
+sleep 2
 cd ../Payload_Development
 
 git clone https://github.com/trustedsec/unicorn.git
