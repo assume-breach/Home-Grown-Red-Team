f621b5df2526d3aaffbf4504babeb4b245716c50
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-03 20:13:20 -0400

    Update AutoC2.sh

--- AutoC2/AutoC2.sh
+++ AutoC2/AutoC2.sh
@@ -84,6 +84,7 @@ cd /opt/Wordlists/
 git clone https://github.com/NotSoSecure/password_cracking_rules.git
 git clone https://github.com/praetorian-inc/Hob0Rules.git
 git clone https://github.com/danielmiessler/SecLists.git
+wget https://raw.githubusercontent.com/praetorian-inc/Hob0Rules/master/wordlists/rockyou.txt.gz
 echo""
 echo "Installing Recon Resources"
 echo""
