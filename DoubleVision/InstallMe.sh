0ef3475ffecf08385e3e04cb7dbefdcd6a38df50
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-16 20:04:01 -0400

    Update InstallMe.sh

--- DoubleVision/InstallMe.sh
+++ DoubleVision/InstallMe.sh
@@ -7,7 +7,7 @@ fi
 
 apt-get update -y && apt-get upgrade -y
 
-apt install tmux apache2 iptables git php dnsmasq apache2 dnsmasq-base python hostapd mdk3 macchanger -y
+apt install tmux apache2 iptables git npm php dnsmasq apache2 dnsmasq-base python hostapd mdk3 macchanger -y
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 
