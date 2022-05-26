49a80855e85064daf8de2d88610fe8f58c044ac9
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-26 11:26:56 -0400

    Update 0365_Captive.sh

--- PhishPi/0365_Captive.sh
+++ PhishPi/0365_Captive.sh
@@ -1,4 +1,13 @@
 #!/bin/bash/
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
 cat << "EOF"
    ___ ____    __ _____   _____                        _                               
   / _ \___ \  / /| ____| |  __ \                      (_)                              
