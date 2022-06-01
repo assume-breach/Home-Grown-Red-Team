8b8c177cdbb61993b2f4172c282ad24d71e53d3c
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-06-01 10:18:50 -0400

    Update cloner.sh

--- PhishPi/cloner.sh
+++ PhishPi/cloner.sh
@@ -19,7 +19,7 @@ read URL
 echo ""
 echo -e ${yellow}"Cloning $URL"${clear}
 /usr/bin/chromium-browser --no-sandbox 2>/dev/null
-runuser -u pi -- ./SingleFile/cli/single-file $URL --browser-executable-path=/usr/bin/chromium-browser /home/pi/index.html
+runuser -u pi -- ./SingleFile/cli/single-file $URL --browser-executable-path=/usr/bin/chromium-browser Cloned_Pages/
 echo ""
 sleep 2
 echo -e ${yellow}"$URL Cloned Successfully"${clear}
