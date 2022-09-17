33805533b9f4616b4fc881639a9e9daa967f412b
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-09-17 09:55:27 -0400

    Update AutoC2.sh
    Fixed dependencies.

--- AutoC2/AutoC2.sh
+++ AutoC2/AutoC2.sh
@@ -37,13 +37,63 @@ sleep 2
 apt --fix-broken install
 apt install git -y
 apt --fix-broken install
-apt install net-tools gparted php-curl nim php-xml docker.io ruby-bundler golang python-pip python3 make snap fuse ruby-bundler python3-pip pipx chromium-browser dnsmasq hostapd openssl open-vm-tools-desktop build-essential libpcap-dev terminator -y
+apt install net-tools -y 
+apt install gparted -y
+apt install php-curl -y 
+apt install php-xml -y
+apt install docker -y
+apt install docker.io -y 
+apt install ruby-bundler -y 
+apt install golang -y
+apt install python-pip -y 
+apt install python3 -y 
+apt install make -y 
+apt install snap -y 
+apt install fuse -y
+apt install ruby-bundler -y 
+apt install pipx -y 
+apt install chromium-browser -y 
+apt install dnsmasq -y 
+apt install hostapd -y 
+apt install openssl -y 
+apt install open-vm-tools-desktop -y
+apt install build-essential -y
+apt install libpcap-dev -y
+apt install terminator -y
 apt --fix-broken install
 echo ""
 echo "Installing Hackery Stuff"
 echo ""
 sleep 2
-apt install nmap wifite nikto wpscan aircrack-ng ettercap-graphical john hashcat crunch tshark macchanger recon-ng snap dhcpd lighttpd mdk4 dsniff mdk3 php-cgi xterm cewl crunch hydra sqlmap ncrack gobuster dirb wfuzz medusa netcat -y
+apt install nmap -y
+apt install wifite -y
+apt install nikto -y
+apt install wpscan -y 
+apt install aircrack-ng -y
+apt install ettercap-graphical -y
+apt install john -y
+apt install hashcat -y
+apt install crunch -y
+apt install tshark -y
+apt install macchanger -y
+apt install recon-ng -y
+apt install dhcpd -y
+apt install lighttpd -y
+apt install mdk4 -y
+apt install dsniff -y
+apt install mdk3 -y
+apt install php-cgi -y
+ap install xterm -y
+apt install cewl -y
+apt install crunch -y
+apt install hydra -y
+apt install sqlmap -y
+apt install ncrack -y
+apt install gobuster -y
+apt install dirb -y
+apt install wfuzz -y
+apt install medusa -y
+apt isntall netcat -y
 apt --fix-broken install
 snap install amass
 echo ""
