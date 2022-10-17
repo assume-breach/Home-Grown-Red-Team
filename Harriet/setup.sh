6d4ef195efad8ddf01fb481b24a5aa9f9cde0ce2
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-10-17 10:11:44 -0400

    Update setup.sh

--- Harriet/setup.sh
+++ Harriet/setup.sh
@@ -3,4 +3,4 @@ cd Harriet/Resources
 apt-get install mingw-w64 -y
 git clone https://github.com/secretsquirrel/SigThief.git
 pip install pycryptodome
-curl -o officedeploymenttool.exe https://www.microsoft.com/en-us/download/confirmation.aspx?id=2fe0642e-4248-4175-94df-3e2a5bc09119
+
