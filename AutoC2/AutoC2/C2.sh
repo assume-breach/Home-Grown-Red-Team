2a610d6ffae9c0d927a546124a774611af4571e7
assume-breach <76174163+assume-breach@users.noreply.github.com>
2023-07-19 08:30:48 -0400

    Update C2.sh

--- AutoC2/AutoC2/C2.sh
+++ AutoC2/AutoC2/C2.sh
@@ -267,6 +267,7 @@ docker build -t covenant .
 
 echo ""
 cd /opt/Command_And_Control/
+echo ""
 echo "Installing Shad0w"
 echo ""
 sleep 2
@@ -279,7 +280,9 @@ echo "Installing Havoc C2"
 echo ""
 git clone https://github.com/HavocFramework/Havoc.git
 echo ""
+sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev python3-dev libboost-all-dev mingw-w64 nasm -y
 sleep 2
+echo ""
 echo "Installing Sliver"
 echo ""
 sleep 2
