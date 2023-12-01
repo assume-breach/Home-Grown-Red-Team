3d991a6a65ce501678256de861f1ada2070b2473
assume-breach <76174163+assume-breach@users.noreply.github.com>
2023-12-01 09:40:05 -0500

    Add files via upload

---
+++ StageFright/StageFright/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update -y
+cd Harriet/Resources
+apt-get install mingw-w64 -y
+git clone https://github.com/secretsquirrel/SigThief.git
+mv SigThief/ Harriet/Resources/
+pip install pycryptodome
+
