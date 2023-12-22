8dcdd85df00081333048e5bb763e02397f02a06f
assume-breach <76174163+assume-breach@users.noreply.github.com>
2023-12-22 16:12:36 -0500

    Update setup.sh

--- Harriet/setup.sh
+++ Harriet/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt update -y
-cd Harriet/Resources
 apt-get install mingw-w64 -y
-git clone https://github.com/secretsquirrel/SigThief.git
-mv SigThief/ Harriet/Resources/
 pip install pycryptodome
-
+apt install osslsigncode -y
+cd Harriet/Resources/
+bash createcert.sh
