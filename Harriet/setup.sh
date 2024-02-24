65cae3e0410ebb473d9f4a2d821511ea15ebf1d1
assume-breach <76174163+assume-breach@users.noreply.github.com>
2024-02-24 08:30:58 -0500

    Update setup.sh

--- Harriet/setup.sh
+++ Harriet/setup.sh
@@ -2,7 +2,7 @@
 apt update -y
 apt-get install mingw-w64 -y
 apt install python3-pip -y
-pip3 install pycryptodome -y
+pip3 install pycryptodome 
 apt install osslsigncode -y
 cd Harriet/Resources/
 bash createcert.sh
