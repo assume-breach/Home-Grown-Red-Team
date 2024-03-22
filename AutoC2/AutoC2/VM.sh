e90be75ce045703e4708a0524880777c0236a3f8
assume-breach <76174163+assume-breach@users.noreply.github.com>
2024-03-22 08:32:22 -0400

    Update VM.sh

--- AutoC2/AutoC2/VM.sh
+++ AutoC2/AutoC2/VM.sh
@@ -5,19 +5,19 @@ echo ""
 sleep 3
 apt-get update -y && apt-get upgrade -y
 apt --fix-broken install -y
-wget https://download.virtualbox.org/virtualbox/6.1.38/Oracle_VM_VirtualBox_Extension_Pack-6.1.38.vbox-extpack
-wget https://download.virtualbox.org/virtualbox/6.1.38/virtualbox-6.1_6.1.38-153438~Ubuntu~jammy_amd64.deb
+wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+wget https://download.virtualbox.org/virtualbox/7.0.14/virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
 apt --fix-broken install -y
-dpkg --install virtualbox-6.1_6.1.34-150636.1~Ubuntu~eoan_amd64.deb
+dpkg --install virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
 echo""
 echo "Downloading Kali VM"
 echo ""
-wget https://kali.download/virtual-images/kali-2022.3/kali-linux-2022.3-virtualbox-amd64.7z
+wget https://cdimage.kali.org/kali-2024.1/kali-linux-2024.1-virtualbox-amd64.7z
 echo ""
 echo "Downloading Windows ISO (Build The Box Yourself And Activate With MassGrave Activation"
 echo ""
 sleep 2
-wget https://pixeldrain.com/api/file/yKpSj3P7?download
+wget https://drive.massgrave.dev/en-us_windows_11_consumer_editions_version_23h2_updated_march_2024_x64_dvd_bcbf6ac6.iso
 echo ""
 echo "Cloning MassGrave's Windows Activation Scripts'"
 git clone https://github.com/massgravel/Microsoft-Activation-Scripts.git
